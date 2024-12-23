61b6697c73d7e5f45e2c50f39df9455b4fbbd24c
Loor Ibrahim <1200495@student.birzeit.edu>
2024-12-23 17:25:47 +0200

    Registration Pages

---
+++ app/src/main/java/com/example/garageapp/Registration.java
@@ -0,0 +1,33 @@
+package com.example.garageapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Registration extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button btnNext = findViewById(R.id.btnNext);
+
+        btnNext.setOnClickListener(v -> {
+            // Start the next activity
+            startActivity(new Intent(Registration.this, Registartion2.class));
+
+        });
+    }
+}
